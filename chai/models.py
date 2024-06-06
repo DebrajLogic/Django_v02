84bcb6879dd8295d97a2be80a14f200a82ad3d6f
DebrajLogic <debrajdey9696@gmail.com>
2024-06-06 05:49:43 +0530

    Added Model and Configured Image Upload Feature

--- chai/models.py
+++ chai/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class ChaiVariety(models.Model):
+    CHAI_TYPE_CHOICE = [
+        ('ML', 'MASALA'),
+        ('GR', 'GINGER'),
+        ('OL', 'OOLONG'),
+        ('PL', 'PLAIN'),
+        ('EL', 'ELAICHI'),
+    ]
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chais/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
